929f2d929327e209b1feda62d5e80cc0de3b4554
Jun Yang <JYang@ucvts.tec.nj.us>
2019-12-09 10:45:17 -0500

    Omae Wa Mou Shindeiru

--- js/shapes.js
+++ js/shapes.js
@@ -20,33 +20,60 @@ window.onload = function() {
 
     document.getElementById("hello").onclick = sayHello;
     document.getElementById("rectangle").onclick = drawRectangle;
-    document.getElementById("colored").onclick = drawColoredRectangle;
-    document.getElementById("traingle").onclick = drawTriangle;
-    document.getElementById("smile").onclick = drawFace;
-    document.getElementById("pyramid").onclick = drawPyramid;
+    document.getElementById("color").onClick = drawColoredRectangle;
+    document.getElementById("triangle").onClick = drawTriangle;
+    document.getElementById("smile").onClick = drawFace;
+    document.getElementById("pyramid").onClick = drawPyramid;
 }
 
-/*
- * Exercise 1.
- */
+ //Exercise 1.
 
 const sayHello = function() {
-    const message = prompt("Message: ");
-    message.style.fontSize = ("48px");
-    message.style.fontFamily = ("sans-serif");
-    message.style.outlineColor = ("black");
-}
+  const canvas = document.getElementById('student-canvas-1');
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  do {
+    var message = prompt("Message: ");
 
-  
+    if (message.length > 50) {
+      alert("Your message is too long. Keep it under 50 characters.");
+    }
+
+  } while (message.length > 50);
+
+  ctx.font = '48px sans-serif';
+  ctx.strokeText(message, 30, 70);
 };
 
-/*
- * Exercise 2.
- */
+//Exercise 2
 
 const drawRectangle = function() {
-    // write your exercise 2 code here
+  const canvas = document.getElementById('student-canvas-2');
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  do {
+    var width = prompt("Width: ");
+    var height = prompt("Height: ");
+    var x = prompt("X: ");
+    var y = prompt("Y: ");
+
+    if (width > 1024 || width < 1) {
+        alert("Your width must be between 1 and 1024.");
+      } else if (height > 512 || height < 1) {
+        alert("Your height must be between 1 and 512.");
+      } else if (x > 1024 || x < 1) {
+        alert("Your x-coordinate must be between 1 and 1024.");
+      } else if (y > 512 || x < 1) {
+        alert("Your y-coordinate must be between 1 and 512.");
+      } else {
+        alert("One of your values is not a number.");
+      }
+
+  } while(width < 1 || width > canvas.width || height < 1 || height > canvas.height || x < 1 || y < 1);
+
+  ctx.strokeRect(x, y, width, height);
 };
 
 /*
@@ -54,7 +81,21 @@ const drawRectangle = function() {
  */
 
 const drawColoredRectangle = function() {
-    // write your exercise 3 code here
+  const canvas = document.getElementById('student-canvas-3');
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  do {
+    var color = String(prompt("Color: ")).toUpperCase();
+
+    if (color != "BLACK" && color != "BLUE" && color != "GREEN" && color != "ORANGE" && color != "PURPLE" && color != "RED" && color != "YELLOW") {
+      alert(color + " is not a supported color.");
+    }
+
+  } while (color != "BLACK" && color != "BLUE" && color != "GREEN" && color != "ORANGE" && color != "PURPLE" && color != "RED" && color != "YELLOW");
+
+  ctx.fillStyle = color;
+  ctx.fillRect(10, 10, 100, 50);
 };
 
 /*
@@ -78,5 +119,20 @@ const drawFace = function() {
  */
 
 const drawPyramid = function() {
-    // write your exercise 5 code here
+  const canvas = document.getElementById('student-canvas-3');
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  do {
+    var block = prompt("Side: ");
+    if (block < 1) {
+      alert("Your block size must be at least 1.");
+    } else if (block > canvas.height || block > canvas.width){
+      alert("Your pyramid won't fit on canvas.");
+    } else {
+      alert("Your block size is not a number.")
+    }
+
+  } while (block < 1 || block > canvas.height || block > canvas.width || isNaN(block));
+
 };
